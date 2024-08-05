d83b75e36a00e2cd1354ff8f430d703569d3124d
KenNyagz <kennyagz681@gmail.com>
2024-08-05 18:04:49 +0300

    aliased commit

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/status', AppController.getStats);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UserController.getMe);
 router.post('/users', UsersController.postNew);
 
 export default router;
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ import routes from './routes/index';
 
 const port = process.env.PORT || 5000;
 const app = express();
+
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ extended: false, limit: '20mb' }));
+
 app.use('/', routes);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
